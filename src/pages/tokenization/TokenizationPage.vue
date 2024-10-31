4198cbcee14c0c9575c53ae7db4f56b18a04a654
coldwindx <15653913658@163.com>
2024-10-31 17:42:48 +0800

    词表构建页面

--- src/pages/tokenization/TokenizationPage.vue
+++ src/pages/tokenization/TokenizationPage.vue
@@ -1,11 +1,54 @@
 <template>
-  <ElSteps style="max-width: 600px" :active="1">
-    <ElStep title="Step 1" :icon="Edit" />
-    <ElStep title="Step 2" :icon="Upload" />
-    <ElStep title="Step 3" :icon="Picture" />
-  </ElSteps>
+  <h1 class="page-title">构建词表</h1>
+  <VaStepper v-model="step" :steps="steps" vertical controls-hidden>
+    <template #step-content-0>
+      <FormDataset></FormDataset>
+    </template>
+
+    <template #step-content-1>
+      <div class="flex flex-col gap-2" style="position: relative">
+        <VaProgressCircle
+          :model-value="process_circle_value"
+          indeterminate
+          color="success"
+          :thickness="0.3"
+          size="480px"
+          class="big_progress_circle"
+        >
+          {{ process_circle_value + '%' }}
+        </VaProgressCircle>
+      </div>
+    </template>
+  </VaStepper>
 </template>
 
-<script lang="ts" setup>
-import { Edit, Picture, Upload } from '@element-plus/icons-vue'
+<script setup>
+import { ref } from 'vue'
+import { defineVaStepperSteps } from 'vuestic-ui'
+import FormDataset from '../tokenization/FormDataset.vue'
+
+const step = ref(0)
+
+const steps = ref(
+  defineVaStepperSteps([
+    {
+      label: '选择数据集',
+      icon: 'store',
+    },
+    {
+      label: '计算TF-IDF',
+      icon: 'local_shipping',
+    },
+    { label: '拓展安全语义分词表', icon: 'done_all' },
+  ]),
+)
+
+const process_circle_value = 75
 </script>
+
+<style>
+.big_progress_circle {
+  --va-progress-circle-position: relative;
+  --va-progress-circle-font-size: 30px;
+}
+</style>
