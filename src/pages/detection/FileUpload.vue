8aaf60e5f3167f4ce18dc3792714efe73679ca98
coldwindx <15653913658@163.com>
2024-11-24 17:17:13 +0800

    文件分片上传&合并

--- src/pages/detection/FileUpload.vue
+++ src/pages/detection/FileUpload.vue
@@ -7,18 +7,18 @@ import axios from 'axios';
 import { ref, defineEmits } from 'vue';
 import { useToast } from 'vuestic-ui'
 
-import { FileSlice, getFileMD5 } from '../../services/fileutils'
+import { NewFileType, FileSlice, getFileMD5 } from '../../services/fileutils'
 
 const { init } = useToast();
 
-// const files = ref([])
+const files = ref<File[]>([])
 const isUploadDisabled = ref(false)
 const headers = { 'Content-Type': 'multipart/form-data' };
 
 // 分片上传
 let chunkSize = 1024 * 1024 * 5
-const files = ref<File[]>([])
 const slices = ref<FileSlice[]>([])
+const promises: unknown[] = []
 
 // 回传父组件
 const emits = defineEmits(["afterUpload"])
@@ -28,11 +28,50 @@ const reset = () => {
     isUploadDisabled.value = false
 }
 
-const upload = async (file: File[]) => {
+const upload = async (files: File[]) => {
+    const file = files[0] as NewFileType
     isUploadDisabled.value = true
-    const hash = await getFileMD5(file[0])
-    // check hash in server
-    const respHash = await axios.post('/api/file/query', { condition: { hash: hash } }).catch((err) => {
+    const hash = await getFileMD5(file) as string
+
+    file.state = {
+        state_txt: 'load',
+        tips_txt: '处理中,请稍后'
+    }
+    file.hash = hash
+    file.progress = 0
+    file.speep = 0
+
+    // 文件切片处理及跳过已上传分片
+    await queryUploadSlice(file, hash)
+    if (slices.length == 0) {
+        file.state = {
+            state_txt: 'success',
+            tips_txt: '上传成功！'
+        }
+        file.progress = 100
+        return
+    }
+
+    // 循环调用切片上传
+    file.state.tips_txt = '上传中，请稍后'
+    for (const slice of slices.value) {
+        fileSliceUploadHandle(slice)
+    }
+
+    await Promise.all(promises).then(() => {
+        // 合并文件
+
+        init({ message: "文件上传成功!", color: 'success' })
+    }).catch((err) => {
+        console.log(err)
+        init({ message: "文件上传失败，系统错误，请联系系统管理员!", color: 'danger' })
+        reset()
+    })
+
+    // 合并文件
+    await mergeSlices(hash)
+    return;
+    const respHash = await axios.post('/api/file/query', { condition: { hash: hash } }).catch(() => {
         init({ message: "文件上传失败，系统错误，请联系系统管理员!", color: 'danger' })
         return reset()
     })
@@ -56,4 +95,98 @@ const upload = async (file: File[]) => {
     return emits("afterUpload", respHash?.data.data[0])
 }
 
+// 查询已上传的切片
+const queryUploadSlice = async (file: File, hash: string) => {
+    await axios.post('/api/slice/query', { condition: { hash: hash }, limit: 0 })
+        .then((res) => {
+            if (0 < res.data.code) {
+                init({ message: "文件上传失败：" + res.data.msg, color: 'danger' })
+                return reset()
+            }
+            let uploadSliceIndexs = res.data.data.map((item: { chunkIndex: any; }) => item.chunkIndex);
+            // 当前文件大小
+            const fileSize = file.size
+            // 当前文件分割多少份 （chunkSize 多少大小分割）
+            let chunkTotal = Math.ceil(fileSize / chunkSize)
+            if (fileSize > chunkSize * 40) {
+                chunkSize = chunkSize * 8
+                chunkTotal = Math.ceil(fileSize / chunkSize)
+            }
+            // split file to slices
+            for (let i = 0; i < chunkTotal; i++) {
+                const startPoint = i * chunkSize
+                const endPoint = Math.min(startPoint + chunkSize, fileSize)
+                if (uploadSliceIndexs.includes(i))
+                    continue;
+                const chunk = file.slice(startPoint, endPoint)
+                // 存储分片状态
+                slices.value.push({
+                    file: chunk,
+                    chunkTotal,
+                    fileName: file.name,
+                    hash,
+                    chunkIndex: i
+                })
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+            init({ message: "文件上传失败，系统错误，请联系系统管理员!", color: 'danger' })
+            reset()
+        })
+}
+
+// 文件切片上传
+const fileSliceUploadHandle = (fileChunk: FileSlice) => {
+    const { chunkTotal, chunkIndex, file, fileName, hash } = fileChunk
+    const formData = new FormData()
+    formData.append('file', file)
+    formData.append('filename', fileName)
+    formData.append('chunkTotal', chunkTotal)
+    formData.append('chunkIndex', chunkIndex)
+    formData.append('hash', hash)
+
+    const promise = new Promise((resolve, reject) => {
+        axios.post("/api/slice/upload", formData, { headers: headers }).then(res => {
+            if (0 < res.data.code) {
+                init({ message: "文件上传失败：" + res.data.msg, color: 'danger' })
+                return reset()
+            }
+            const file = files.value[0] as NewFileType
+            file.speep = 100 / Number(chunkTotal)
+            file.progress += file.speep
+            if (file.progress > 100) {
+                file.progress = 100
+            }
+            if (Math.ceil(file.progress) >= 100) {
+                file.state = {
+                    state_txt: 'success',
+                    tips_txt: '上传成功！'
+                }
+            }
+            return resolve(true)
+        }).catch((err) => {
+            console.log(err)
+            init({ message: "文件上传失败，系统错误，请联系系统管理员!", color: 'danger' })
+            reset()
+        })
+    })
+    promises.push(promise)
+}
+
+const mergeSlices = async (hash: string) => {
+    await axios.post('/api/slice/merge', {hash:hash})
+        .then((res) => {
+            if (0 < res.data.code) {
+                init({ message: "文件合并失败：" + res.data.msg, color: 'danger' })
+                return reset()
+            }
+            return emits("afterUpload", res?.data.data)
+        })
+        .catch((err) => {
+            console.log(err)
+            init({ message: "文件上传失败，系统错误，请联系系统管理员!", color: 'danger' })
+            reset()
+        })
+}
 </script>
--- src/services/fileutils.ts
+++ src/services/fileutils.ts
@@ -19,7 +19,7 @@ export interface FileSlice {
     file: Blob
     hash: string
     chunkIndex: number
-    chunkCount: string
+    chunkTotal: number
     fileName: string
 }
 
